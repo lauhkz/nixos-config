97360ba97097bd52700f1fc2533d176559315b37
lautar0pp <lautaro_RL@outlook.com>
2024-04-26 08:25:48 -0300

    added i3status conf

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, config, ...}: 
+{ pkgs, config, ...}:
 
 {
   # wallpaper, binary file
   home.file.".config/i3/wallpaper-boshi.jfif".source = ./wallpaper-boshi.jfif;
   home.file.".config/i3/config".source = ./config;
+  home.file.".config/i3status/config".source = ./i3status.conf;
   #home.file.".config/i3/i3blocks.conf".source = ./i3blocks.conf;
   #home.file.".config/i3/keybindings".source = ./keybindings;
   home.file.".config/i3/scripts" = {
